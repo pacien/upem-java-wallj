8d21e99bbb3131b3208ab8b05e30050e8ba183c6
pacien <pacien.trangirard@pacien.net>
2018-01-07 22:40:05 +0100

    Change source tree
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

---
+++ src/main/java/fr/umlv/java/wallj/Main.java
@@ -0,0 +1,20 @@
+package fr.umlv.java.wallj;
+
+import fr.umlv.zen5.Application;
+
+import java.awt.*;
+
+public class Main {
+
+  /**
+   * Dummy entry point.
+   *
+   * @param args no need to argue
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    Application.run(Color.BLACK, (ac) -> {
+    });
+  }
+
+}
