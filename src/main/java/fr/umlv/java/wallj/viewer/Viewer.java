0c03018b37bceb81bcefcd89288e9baaea64a2c2
Adam NAILI <a.naili@live.fr>
2018-01-09 01:28:59 +0100

    Modifying the signature of method to provide ApplicationContext to render

--- src/main/java/fr/umlv/java/wallj/viewer/Viewer.java
+++ src/main/java/fr/umlv/java/wallj/viewer/Viewer.java
@@ -4,17 +4,31 @@ import fr.umlv.zen5.ApplicationContext;
 
 import java.awt.*;
 
+/**
+ * Link between application and Zen 5
+ */
 public class Viewer {
 
   public static void main(String[] strings) {
 
   }
 
+  /**
+   *
+   * @param applicationContext the application context from Zen 5
+   */
   public static void eventLoop(ApplicationContext applicationContext) {
     //TODO lambda exp for Application.run(Consumer<...>)
+    //usage : Viewer::eventLoop
   }
 
-  public static void renderFrame(Graphics2D graphics2D) {
+  /**
+   *
+   * @param graphics2D the graphic2D from Zen 5
+   * @param applicationContext the application context from Zen 5
+   */
+  public static void renderFrame(Graphics2D graphics2D,ApplicationContext applicationContext) {
     //TODO lambda exp for ApplicationContext.renderFrame(Consumer<...>)
+    //usage : graphics->Viewer.renderFrame(graphics2D,applicationContext)
   }
 }
