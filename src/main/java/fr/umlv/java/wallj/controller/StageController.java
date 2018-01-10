474444d4fad7c6c86ae01a2c208d330840283665
pacien <pacien.trangirard@pacien.net>
2018-01-10 19:44:15 +0100

    Add event return
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

--- src/main/java/fr/umlv/java/wallj/controller/StageController.java
+++ src/main/java/fr/umlv/java/wallj/controller/StageController.java
@@ -1,12 +1,16 @@
 package fr.umlv.java.wallj.controller;
 
 import fr.umlv.java.wallj.context.Context;
+import fr.umlv.java.wallj.event.Event;
+
+import java.util.List;
 
 public class StageController implements Controller {
   //TODO
 
   @Override
-  public void update(Context context) {
-
+  public List<Event> update(Context context) {
+    return null;
   }
+
 }
