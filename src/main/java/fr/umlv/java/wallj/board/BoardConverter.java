1347f30afbed7ee9c149f77534f5e75fbe819b98
Adam NAILI <adam.naili@etud.u-picardie.fr>
2018-01-11 20:07:28 +0100

    Implementing BoardConverter and test failed

--- src/main/java/fr/umlv/java/wallj/board/BoardConverter.java
+++ src/main/java/fr/umlv/java/wallj/board/BoardConverter.java
@@ -1,5 +1,43 @@
 package fr.umlv.java.wallj.board;
 
-public class BoardConverter {
+import fr.umlv.java.wallj.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Adam NAILI
+ */
+public final class BoardConverter {
   //TODO
+  private BoardConverter() {
+  }
+
+  public static Board worldToBoard(List<Block> blocks) {
+    int width = blocks.stream().map(Block::getTile).mapToInt(TileVec2::getRow).max().orElse(-1) + 1;
+    int height = blocks.stream().map(Block::getTile).mapToInt(TileVec2::getCol).max().orElse(-1) + 1;
+
+    Board.Builder builder = new Board.Builder(width, height);
+    for (Block block : blocks) {
+      builder.setBlockTypeAt(block.getTile(), block.getType());
+    }
+    return builder.build();
+  }
+
+  public static List<Block> boardToWorld(Board board) {
+    ArrayList<Block> blocks = new ArrayList<>();
+    int nbRow = board.getDim().getRow();
+    int nbCol = board.getDim().getCol();
+    for (int i = 0; i < nbRow; i++) {
+      for (int j = 0; i < nbCol; j++) {
+        Block block;
+        TileVec2 location = TileVec2.of(i, j);
+        block = BlockFactory.build(board.getBlockTypeAt(location), location);
+        if (block != null) {
+          blocks.add(block);
+        }
+      }
+    }
+    return blocks;
+  }
 }
--- src/test/java/fr/umlv/java/wallj/board/BoardConverterTest.java
+++ src/test/java/fr/umlv/java/wallj/board/BoardConverterTest.java
@@ -1,4 +1,48 @@
 package fr.umlv.java.wallj.board;
 
-public class BoardConverterTest {
+import fr.umlv.java.wallj.model.*;
+import org.jbox2d.common.Vec2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Adam NAILI
+ */
+final class BoardConverterTest {
+
+  @Test
+  void testWorldToBoard(){
+    List<Block> blocks = new LinkedList<>();
+    Board.Builder builder = new Board.Builder(5,1);
+    TileVec2 t0 = TileVec2.of(0,0);
+    TileVec2 t1 = TileVec2.of(1,0);
+    TileVec2 t2 = TileVec2.of(2,0);
+    TileVec2 t3 = TileVec2.of(3,0);
+    TileVec2 t4 = TileVec2.of(4,0);
+
+    builder.setBlockTypeAt(t0,BlockType.WALL);
+    builder.setBlockTypeAt(t1,BlockType.BOMB);
+    builder.setBlockTypeAt(t2,BlockType.GARBAGE);
+    builder.setBlockTypeAt(t3,BlockType.ROBOT);
+    builder.setBlockTypeAt(t4,BlockType.TRASH);
+
+    Board certifiedBoard = builder.build();
+
+    blocks.add(BlockFactory.build(BlockType.WALL,t0));
+    blocks.add(BlockFactory.build(BlockType.BOMB,t1));
+    blocks.add(BlockFactory.build(BlockType.GARBAGE,t2));
+    blocks.add(BlockFactory.build(BlockType.ROBOT,t3));
+    blocks.add(BlockFactory.build(BlockType.TRASH,t4));
+
+    Board board = BoardConverter.worldToBoard(blocks);
+
+    Assertions.assertEquals(certifiedBoard,board);
+
+  }
+
 }
+
