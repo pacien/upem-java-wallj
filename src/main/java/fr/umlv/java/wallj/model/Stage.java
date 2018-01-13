9decc968bee4b455d96472b51bf8490275a12df4
Adam NAILI <a.naili@live.fr>
2018-01-14 00:09:54 +0100

    Implementing very basics stuffs in Stage to unlock situation in other classes

--- src/main/java/fr/umlv/java/wallj/model/Stage.java
+++ src/main/java/fr/umlv/java/wallj/model/Stage.java
@@ -1,5 +1,36 @@
 package fr.umlv.java.wallj.model;
 
+import fr.umlv.java.wallj.board.Board;
+import fr.umlv.java.wallj.context.Context;
+import fr.umlv.java.wallj.event.Event;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author
+ */
 public class Stage {
   //TODO Class Stage
+  private final Board currentBoard;
+
+  public Stage(Board board) {
+    currentBoard = Objects.requireNonNull(board);
+  }
+
+  /**
+   * @return the current board of the game
+   */
+  public Board getCurrentBoard() {
+    return currentBoard;
+  }
+
+  /**
+   * @param context the current context
+   * @return a list of new events to perform
+   */
+  public List<Event> update(Context context) {
+    //TODO
+  }
+
 }
