c0dbbdc990a68a99b2d913bba9698d7e0894e924
Adam NAILI <a.naili@live.fr>
2018-01-14 22:48:02 +0100

    Modifying the implementation of the Main, with Validation feature and exception handling

--- src/main/java/fr/umlv/java/wallj/Main.java
+++ src/main/java/fr/umlv/java/wallj/Main.java
@@ -2,6 +2,7 @@ package fr.umlv.java.wallj;
 
 import fr.umlv.java.wallj.board.Board;
 import fr.umlv.java.wallj.board.BoardParser;
+import fr.umlv.java.wallj.board.BoardValidator;
 import fr.umlv.java.wallj.viewer.Viewer;
 import fr.umlv.zen5.Application;
 
@@ -32,20 +33,31 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    java.util.List<Board> boards = new LinkedList<>();
+    List<Board> boards = new LinkedList<>();
     try {
       List<Path> boardPaths = Main.getResourcePaths(args);
       for (Path path : boardPaths) {
-        boards.add(BoardParser.parse(path));
+        BoardValidator boardValidator = new BoardValidator(BoardParser.parse(path));
+        boards.add(
+         boardValidator.validate(BoardValidator.Constraint::hasActualReachableBlocks, "Some supposed reachable blocks are not reachable.")
+          .validate(BoardValidator.Constraint::hasMandatoryBlocks, "Some mandatory blocks are missing.")
+          .validate(BoardValidator.Constraint::isBounded, "The board is not correctly bounded.")
+          .validate(BoardValidator.Constraint::isHollow, "The board must have a unique and simple interior.")
+          .get());
       }
       Viewer viewer = new Viewer(boards);
-      Application.run(Color.BLACK, viewer::eventLoop);
+      Application.run(Color.WHITE, viewer::eventLoop);
     } catch (URISyntaxException e) {
       System.err.println(e.getMessage());
       System.exit(1);
     } catch (IOException e) {
       System.err.println(e.getMessage());
       System.exit(2);
+    } catch (BoardValidator.ValidationException e) {
+      for (Throwable throwable : e.getSuppressed()) {
+        System.err.println(throwable.getMessage());
+      }
+      System.exit(3);
     }
   }
 }
