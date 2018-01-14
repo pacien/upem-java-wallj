b54e20222747a12621cdc31e6ee78f841726d14c
Adam NAILI <a.naili@live.fr>
2018-01-14 15:20:19 +0100

    Adding 'isOver' feature to GameController

--- src/main/java/fr/umlv/java/wallj/controller/GameController.java
+++ src/main/java/fr/umlv/java/wallj/controller/GameController.java
@@ -1,16 +1,39 @@
 package fr.umlv.java.wallj.controller;
 
 import fr.umlv.java.wallj.context.Context;
+import fr.umlv.java.wallj.context.Game;
+import fr.umlv.java.wallj.event.ConfirmEvent;
 import fr.umlv.java.wallj.event.Event;
+import fr.umlv.java.wallj.event.GameOverEvent;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class GameController implements Controller {
-  //TODO
 
   @Override
   public List<Event> update(Context context) {
-    return null;
+    boolean isConfirmEvent = context.getEvents().stream().anyMatch(event -> event instanceof ConfirmEvent);
+    boolean isGameOver = context.getEvents().stream().allMatch(event -> event instanceof)
+    Game currentGame = context.getGame();
+    LinkedList<Event> events = new LinkedList<>();
+    if (isConfirmEvent) {
+      //TODO exiting
+      if (!isGameOver) {
+        if (currentGame.getCurrentStage().isCleared()) {
+          if (currentGame.hasNextBoard()) { //continue
+            currentGame.nextStage();
+          } else {
+            events.add(new GameOverEvent());
+          }
+        } else {//retry
+          currentGame.retryStage();
+        }
+      } else {
+        currentGame.setOver();
+      }
+    }
+    return events;
   }
 
 }
---
+++ src/main/java/fr/umlv/java/wallj/event/GameOverEvent.java
@@ -0,0 +1,4 @@
+package fr.umlv.java.wallj.event;
+
+public class GameOverEvent {
+}
