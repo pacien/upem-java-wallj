f0f1352d94c56ab76d1ba0e7a1dce7ac000dfd62
pacien <pacien.trangirard@pacien.net>
2018-02-03 23:31:36 +0100

    Add simulation start event
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

---
+++ src/main/java/fr/umlv/java/wallj/event/SimulationStartEvent.java
@@ -0,0 +1,10 @@
+package fr.umlv.java.wallj.event;
+
+/**
+ * Signals the beginning of the physics simulation.
+ *
+ * @author Pacien TRAN-GIRARD
+ */
+public final class SimulationStartEvent implements GameEvent {
+  // void
+}
