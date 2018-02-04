1ff3d2a5fc2a1465ca3e0ad5b55d910b7c720368
pacien <pacien.trangirard@pacien.net>
2018-02-04 19:17:56 +0100

    Move contact edge stream creation
    Signed-off-by: pacien <pacien.trangirard@pacien.net>

--- src/main/java/fr/umlv/java/wallj/block/JBoxBlock.java
+++ src/main/java/fr/umlv/java/wallj/block/JBoxBlock.java
@@ -3,10 +3,17 @@ package fr.umlv.java.wallj.block;
 import org.jbox2d.collision.shapes.Shape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.*;
+import org.jbox2d.dynamics.contacts.ContactEdge;
 
 import java.util.Objects;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
+ * A block linked to a physical body.
+ *
  * @author Pacien TRAN-GIRARD
  */
 public abstract class JBoxBlock extends Block {
@@ -25,6 +32,43 @@ public abstract class JBoxBlock extends Block {
    */
   public Body getBody() {
     return body;
+  }
+
+  /**
+   * @return a stream of contact edges of this body
+   */
+  public Stream<ContactEdge> streamContactEdges() {
+    // If only we had (a working) Java 9...
+    // return Stream.iterate(getBody().getContactList(), c -> c.next)
+    //        .takeWhile(Objects::nonNull);
+
+    return StreamSupport.stream(new Spliterator<ContactEdge>() {
+      private ContactEdge contactEdge = getBody().getContactList();
+
+      @Override
+      public boolean tryAdvance(Consumer<? super ContactEdge> consumer) {
+        if (contactEdge == null) return false;
+
+        consumer.accept(contactEdge);
+        contactEdge = contactEdge.next;
+        return true;
+      }
+
+      @Override
+      public Spliterator<ContactEdge> trySplit() {
+        return null;
+      }
+
+      @Override
+      public long estimateSize() {
+        return Long.MAX_VALUE;
+      }
+
+      @Override
+      public int characteristics() {
+        return NONNULL;
+      }
+    }, false);
   }
 
   @Override
--- src/main/java/fr/umlv/java/wallj/block/TrashBlock.java
+++ src/main/java/fr/umlv/java/wallj/block/TrashBlock.java
@@ -7,13 +7,9 @@ import fr.umlv.java.wallj.event.BlockDestroyEvent;
 import fr.umlv.java.wallj.event.Event;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.contacts.ContactEdge;
 
 import java.awt.*;
-import java.util.Spliterator;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
  * A trash block.
@@ -42,39 +38,5 @@ public class TrashBlock extends JBoxBlock {
   private Stream<Event> paint(Context context) {
     context.getGraphicsContext().paintRectangle(Color.RED, getPos(), TileVec2.TILE_DIM, TileVec2.TILE_DIM);
     return Stream.empty();
-  }
-
-  private Stream<ContactEdge> streamContactEdges() {
-    // If only we had (a working) Java 9...
-    // return Stream.iterate(getBody().getContactList(), c -> c.next)
-    //        .takeWhile(Objects::nonNull);
-
-    return StreamSupport.stream(new Spliterator<ContactEdge>() {
-      private ContactEdge contactEdge = getBody().getContactList();
-
-      @Override
-      public boolean tryAdvance(Consumer<? super ContactEdge> consumer) {
-        if (contactEdge == null) return false;
-
-        consumer.accept(contactEdge);
-        contactEdge = contactEdge.next;
-        return true;
-      }
-
-      @Override
-      public Spliterator<ContactEdge> trySplit() {
-        return null;
-      }
-
-      @Override
-      public long estimateSize() {
-        return Long.MAX_VALUE;
-      }
-
-      @Override
-      public int characteristics() {
-        return NONNULL;
-      }
-    }, false);
   }
 }
